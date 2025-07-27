6c271dbc21915a33b30ae8eca156cf2d789e11b8
Corrin Lakeland <corrin.lakeland@countdown.co.nz>
2025-07-27 13:42:45 +1200

    Build scripts so you can build from the command line.

---
+++ build_dev.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" OpenDental.sln /p:Configuration=Debug /p:Platform=x86 /p:WarningLevel=0 /v:q
---
+++ build_release.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" OpenDental.sln /p:Configuration=Release /v:q
